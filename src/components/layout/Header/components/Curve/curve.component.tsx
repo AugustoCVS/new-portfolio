66b967b5a9fa9770dc3deefd4c4dec337692abde
AugustoCVS <guty.vertolis@gmail.com>
2024-05-21 17:24:46 -0300

    feat: create curve component

---
+++ src/components/layout/Header/components/Curve/curve.component.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+
+export const Curve: React.FC = () => {
+  const initialPath = `M100 0 L200 0 L200 ${window.innerHeight} L100 ${
+    window.innerHeight
+  } Q-100 ${window.innerHeight / 2} 100 0`;
+
+  const targetPath = `M100 0 L200 0 L200 ${window.innerHeight} L100 ${
+    window.innerHeight
+  } Q100 ${window.innerHeight / 2} 100 0`;
+
+  const curve = {
+    initial: {
+      d: initialPath,
+    },
+    enter: {
+      d: targetPath,
+      transition: { duration: 1, ease: [0.76, 0, 0.24, 1] },
+    },
+    exit: {
+      d: initialPath,
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+    },
+  };
+
+  return (
+    <svg className="absolute top-0 left-[-99px] w-[100px] h-full min-h-screen stroke-none fill-gray-600">
+      <motion.path
+        variants={curve}
+        initial="initial"
+        animate="enter"
+        exit="exit"
+      ></motion.path>
+    </svg>
+  );
+};
