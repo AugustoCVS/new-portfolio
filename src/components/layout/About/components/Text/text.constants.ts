20ac042329583e93b929e1fe59fa52f1bbf56ca3
AugustoCVS <guty.vertolis@gmail.com>
2024-05-21 20:23:46 -0300

    refactor: change text

--- src/components/layout/About/components/Text/text.constants.ts
+++ src/components/layout/About/components/Text/text.constants.ts
@@ -1,10 +1,10 @@
 export const DescriptionText = [
   {
     id: 0,
-    text: "Desenvolvedor fullstack com experiência em projetos de pequeno, médio e grande porte, conhecimento consolidado em React, React Native, Typescript, Redux, NextJs e Jest",
+    text: "Desenvolvedor fullstack com experiência em projetos de pequeno, médio e grande porte, conhecimento consolidado em React, React Native, Typescript, Redux, NextJs, Node, Nest, Jest e Testing Library.",
   },
   {
     id: 1,
-    text: "Experiência em desenvolvimento de aplicações web e mobile, integração com APIs, desenvolvimento de testes automatizados, manutenção de aplicações legadas e desenvolvimento de novas funcionalidades",
+    text: "Experiência em desenvolvimento de aplicações web e mobile, integração com APIs, desenvolvimento de testes automatizados, manutenção de aplicações legadas e desenvolvimento de novas funcionalidades.",
   },
 ];
