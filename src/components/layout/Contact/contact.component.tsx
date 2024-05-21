1518daaa2a496bb18c70029fc946680d369ca4c0
AugustoCVS <guty.vertolis@gmail.com>
2024-05-21 20:24:06 -0300

    feat: add social media component

--- src/components/layout/Contact/contact.component.tsx
+++ src/components/layout/Contact/contact.component.tsx
@@ -1,14 +1,18 @@
 import { Information } from "@/components/common/Information";
 import { TITLE_MESSAGE } from "./contact.constants";
 import { Form } from "./components/form/form.component";
+import { SocialMedia } from "./components/SocialMedia/social-media.component";
 
 export const Contact: React.FC = () => {
   return (
     <Information.Root id="contact">
-      <Information.TextWrapper>
-        <Information.Title text={TITLE_MESSAGE} dataAos="fade-up" />
+      <Information.TextWrapper dataAos="fade-up">
+        <Information.Title text={TITLE_MESSAGE} />
       </Information.TextWrapper>
       <Form />
+      <Information.Wrapper dataAos="fade-up">
+        <SocialMedia />
+      </Information.Wrapper>
     </Information.Root>
   );
 };
